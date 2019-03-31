69dd9e5462b4468948e49942584df51d9a4e3b25
Stefan Edlich <edlich@gmail.com>
2019-03-31 20:36:35 +0200

    finished random play

--- src/control.py
+++ src/control.py
@@ -1,5 +1,6 @@
 # (C) 2016, Stefan Edlich, Apache 2 License
 import chess
+from random import randint
 
 mtime = 0 # movetime set by the 
 goparams = [] # Could be ...
@@ -13,19 +14,13 @@ def cmain(tuple): # board, 1462484917.8764188, ['c2c4', 'c7c5'],['depth', '4']
 
   board.push(chess.Move.from_uci(line[-1]))
   print(board)
-  print("-------------------")
+  print("-------------------\n")
 
-  counter = 0
-  mlist = []
-  for move in board.legal_moves:
-    counter += 1
-    mlist.append(move)
+  mlist = list(board.legal_moves)
+  print("type=",type(mlist))
+  print("type=",len(mlist))
+  the_move = mlist[randint(0,len(mlist))-1]
 
-  print("counter=", counter)
-  print("move 5=", mlist[5])
-
-  board.push(mlist[0])
-  print("bestmove", "e7e6") # mlist.len()-2
+  board.push(the_move)
+  print("bestmove", the_move)
   print(board)
-
-
